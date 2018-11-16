fd6d857b8abebfc9ed3136370fe2a9cbda65a96c
SarahGarda <sarah.garda@student-cs.fr>
2018-11-16 11:29:28 +0100

    minor update

--- tweet_text2/sentiment_analysis.py
+++ tweet_text2/sentiment_analysis.py
@@ -5,16 +5,28 @@ from twitter_collect.twitter_collection import *
 file=collect()
 
 def sentiment_percentage(data):
+    '''
+    :param data: the file to analyze
+    :return: the percentage of negative, positive and neutral tweets in data
+    '''
     pos_tweets=[]
     neu_tweets=[]
     neg_tweets=[]
     for tweet in data:
-        if TextBlob(tweet.text).sentiment.polarity<-0.33:
-            neg_tweets.append(tweet.text)
-        elif TextBlob(tweet.text).sentiment.polarity>0.33:
-            pos_tweets.append(tweet.text)
+        tweet_en=TextBlob(tweet.text).translate(from_lang='fr',to='en')
+        #if the tweets stay in french textblob.sentiment returns 0 as polarity and 0 as subjectivity
+        if tweet_en.sentiment.subjectivity<0.5:
+            if tweet_en.sentiment.polarity<-0.33:
+                neg_tweets.append(tweet.text)
+            elif tweet_en.sentiment.polarity>0.33:
+                pos_tweets.append(tweet.text)
+            else:
+                neu_tweets.append(tweet.text)
         else:
-            neu_tweets.append(tweet.text)
+            if tweet_en.sentiment.polarity<0:
+                neg_tweets.append(tweet.text)
+            else:
+                pos_tweets.append(tweet.text)
     print("Percentage of positive tweets: {}%".format(len(pos_tweets)*100/len(data)))
     print("Percentage of neutral tweets: {}%".format(len(neu_tweets)*100/len(data)))
     print("Percentage de negative tweets: {}%".format(len(neg_tweets)*100/len(data)))
