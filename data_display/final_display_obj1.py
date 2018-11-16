5a60301cbd6ef6bfe24e1fd71e6ab0e9dacf1d1e
SarahGarda <sarah.garda@student-cs.fr>
2018-11-16 14:18:09 +0100

    minor update

--- data_display/final_display_obj1.py
+++ data_display/final_display_obj1.py
@@ -3,16 +3,22 @@ from tweet_text2.sentiment_analysis import *
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-sns.set(style="white", context="talk")
-f, (ax1) = plt.subplots(1, 1, figsize=(7, 5), sharex=True)
-y=sentiment_percentage(file)
-x=['Positive tweets', 'Neutral tweets', 'Negative tweets']
-sns.barplot(x=x, y=y, palette="rocket", ax=ax1)
-ax1.axhline(0, color="k", clip_on=False)
-ax1.set_ylabel("Percentage")
-sns.despine(bottom=True)
-plt.setp(f.axes, yticks=[])
-plt.tight_layout(h_pad=2)
+def tweet_polarity():
+    '''
+    :return: the graphs of the percentage of positive, negative and neutral tweets
+    '''
+    sns.set(style="white", context="talk")
+    f, (ax1) = plt.subplots(1, 1, figsize=(7, 5), sharex=True)
+    y=sentiment_percentage(file)
+    x=['Positive tweets', 'Neutral tweets', 'Negative tweets']
+    sns.barplot(x=x, y=y, palette="rocket", ax=ax1)
+    ax1.axhline(0, color="k", clip_on=False)
+    ax1.set_ylabel("Percentage")
+    sns.despine(bottom=True)
+    plt.setp(f.axes, yticks=[])
+    plt.tight_layout(h_pad=2)
+    plt.show()
 
-interest_graph()
 
+print(interest_graph())
+print(tweer_polarity)
