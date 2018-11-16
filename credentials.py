cc3419f3d51d86a52cd69d635225c48b3f83442c
SarahGarda <45000500+SarahGarda@users.noreply.github.com>
2018-11-16 16:01:53 +0100

    Delete credentials.py

--- credentials.py
+++
@@ -1,9 +0,0 @@
-# Twitter App access keys for @user
-
-# Consume:
-CONSUMER_KEY    = 'UZxtFNp1KWCvejtitRBVCwEZi'
-CONSUMER_SECRET = 'gry0UmYyABcUkWSjQicLNTRcoyCeNbRhkdhIcqY6R5gL6UpdGx'
-
-# Access:
-ACCESS_TOKEN  = '1062264883209822208-ghg8PhQju17dZGwLzjIemV5zQmJm3D'
-ACCESS_SECRET = '3gSYHiPSAcfsjaAycCMiUyWqe22ai6GE2hZdRWIvfaZMU'
